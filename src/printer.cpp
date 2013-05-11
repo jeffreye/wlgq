d9db9c49d59d88044b13117524cf5ab2db26fa55
jeffreye <lightning.x.revenant@gmail.com>
2013-05-11 13:16:33 +0800

    view content of file

--- src/printer.cpp
+++ src/printer.cpp
@@ -1,6 +1,9 @@
 #include "printer.h"
 #include <boost/bind.hpp>
 #include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
 
 printer::printer()
 {
@@ -15,20 +18,35 @@ printer::~printer()
 void printer::printAll(query &result)
 {
     //now just print raw result
-    std::for_each(result.results.begin(),result.results.end(),boost::bind(boost::mem_fn(&printer::print),this,_1));
+    result.for_each(boost::bind(boost::mem_fn(&printer::print),this,_1));
     cout<<"QUERY:"<<result.get_query_words()<<endl;
-    cout<<"AMOUNT:"<<result.results.size()<<endl;
+    cout<<"about "<<result.size()<<" results"<<endl;
 }
 
 void printer::print(word_position *pos)
 {
-    cout<<"File:"<<pos->document<<"\t\t\tPosition:"<<pos->position<<endl;
+    cout<<"File:"<<pos->document<<endl;
+
+    ifstream fin;
+    fin.open(pos->document);
+    fin.seekg(pos->position);
+    int line=3;
+    string l;
+    while (line-->0)
+    {
+        getline(fin,l);
+        hightlight(l,35);
+        cout<<endl;
+    }
+    fin.close();
+
+    cout<<endl;
 }
 
 
 void printer::hightlight(string s,int color,bool bold)
 {
-    cout << "\033["<< (bold?"1":"0") <<";" << color << "mHello!\033[0m";
+    cout << "\033["<< (bold?"1":"0") <<";" << color << "m"<<s<<"\033[0m";
 
 //    for (int i = 31; i <= 37; i++)
 //{
